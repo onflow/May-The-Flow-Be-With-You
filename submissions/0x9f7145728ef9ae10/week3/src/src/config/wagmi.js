65d97ab25181bd94e13c5c3c4f0decf1969265fd
Tan Zhi Xuan <69501009+Zhixuan0318@users.noreply.github.com>
2025-05-25 00:37:22 +0800

    Delete submissions/0x9f7145728ef9ae10/week3 directory

--- submissions/0x9f7145728ef9ae10/week3/src/src/config/wagmi.js
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { flowTestnet } from '@wagmi/core/chains';
-
-export const config = getDefaultConfig({
-  appName: 'doodle',
-  projectId: '0d96c994eeaf761d2d2ac3a07192d980',
-  chains: [flowTestnet],
-  ssr: true,
-});
